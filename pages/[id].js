c19e670517ceb1e91ca728ff92e119cbb44fb7a2
Finn Chau <khoacd@smartosc.com>
2022-07-26 18:00:30 +0700

    modified

---
+++ pages/[id].js
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+  // Chỉ pre-render 5 post đầu
+  const paths = posts.slice(0, 5).map((post) => ({
+    params: {
+      id: post.id.toString(),
+    },
+  }));
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+}
+
+export default function Home({ posts }) {
+  return posts.map((post) => (
+    <div key={post.id}>
+      <Link href={`/${post.id}`}>
+        <a>
+          {post.id}. {post.title}
+        </a>
+      </Link>
+    </div>
+  ));
+}
