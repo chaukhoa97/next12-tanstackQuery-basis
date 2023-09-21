53d9ef3014a913ce7e0d66786947cf8a81a61281
Finn Chau <jonhkane00@gmail.com>
2023-09-21 23:35:40 +0700

    minor change

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,34 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+
+const PercentageLineLoader = () => {
+  const [percentage, setPercentage] = useState(0)
+
+  useEffect(() => {
+    // Simulate a loading process by increasing the percentage over time
+    const interval = setInterval(() => {
+      if (percentage < 100) {
+        setPercentage((prevPercentage) => prevPercentage + 1)
+      }
+    }, 100)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [percentage])
+
+  return (
+    <div className="relative h-8 w-full bg-gray-300 rounded-full">
+      <div
+        className="absolute top-0 left-0 h-full bg-blue-500 rounded-full"
+        style={{ width: `${percentage}%` }}
+      />
+      <div className="absolute inset-0 flex items-center justify-center text-gray-700 font-semibold">
+        {percentage}%
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
@@ -15,6 +45,7 @@ export default function Home() {
       <Link className="block" href="/react-query-with-next">
         React Query with Next.js
       </Link>
+      <PercentageLineLoader />
     </>
   )
 }
